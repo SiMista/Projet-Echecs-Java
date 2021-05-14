58af5fe0271b60e1c19c8fdee61a1806896004a5
SiMista <78701925+SiMista@users.noreply.github.com>
2021-05-14 15:50:13 +0200

    insertion des ErreurDpleactement et Erreur saisie

--- src/jeu/Echiquier.java
+++ src/jeu/Echiquier.java
@@ -61,13 +61,64 @@ public class Echiquier {
 		return s;
 	}
 
+	public boolean erreurSaisie(String s) {
+		String problème = "";
+		boolean erreur = false;
+
+		if (s.length() != 4) {
+			System.out.println("La saisie doit faire 4 caractères");
+			return true;
+		}
+		if (!Character.isLetter(s.charAt(0)) || !Character.isLetter(s.charAt(2))) {
+			problème += "Le 1er et le 3ème caractère doivent être des lettres\n";
+			erreur = true;
+		}
+		if (!Character.isDigit(s.charAt(1)) || !Character.isDigit(s.charAt(3))) {
+			problème += "Le 2ème et le 4ème caractère doivent être des chiffres";
+			erreur = true;
+		}
+		if (problème != "")
+			System.out.println(problème);
+		return erreur;
+	}
+
+	public boolean erreurDéplacement(int ligneA, int colonneA, int ligneD, int colonneD) {
+		String problème = "";
+		boolean erreur = false;
+
+		if (ligneA < 0 || ligneA >= MAX || ligneD < 0 || ligneD >= MAX) {
+			problème += "Une ou plusieurs lignes saisies ne sont pas dans l'échiquier\n";
+			erreur = true;
+		}
+		if (colonneA < 0 || colonneA >= MAX || colonneD < 0 || colonneD >= MAX) {
+			problème += "Une ou plusieurs colonnes saisies ne sont pas dans l'échiquier\n";
+			erreur = true;
+		}
+		if (estLibre(ligneA, colonneA)) {
+			problème += "La case actuelle est vide\n";
+			erreur = true;
+		}
+		if (ligneA == ligneD && colonneA == colonneD) {
+			problème += "Les pièces ne peuvent pas se déplacer à la meme place";
+			erreur = true;
+		}
+		if (problème != "")
+			System.out.println(problème);
+		return erreur;
+	}
+
 	public boolean jouer(String s, Couleur c) {
 
+		if (erreurSaisie(s))
+			return false;
 		int colonneA = s.charAt(0) - ConversASCII - 1;
 		int ligneA = MAX - Integer.parseInt(s.substring(1, 2));
 		int colonneD = s.charAt(2) - ConversASCII - 1;
 		int ligneD = MAX - Integer.parseInt(s.substring(3, 4));
 
+		if (erreurDéplacement(ligneA, colonneA, ligneD, colonneD)) {
+			return false;
+		}
 		if (plateau[ligneA][colonneA].getCouleur() != c) {
 			System.out.println("Vous ne pouvez pas jouer des pièces adverse");
 			return false;
@@ -79,28 +130,21 @@ public class Echiquier {
 				return false;
 			}
 		}
-
-		if ((colonneA >= 0 && colonneA <= MAX && ligneA >= 0 && ligneA <= MAX)
-				&& (colonneD >= 0 && colonneD <= MAX && ligneD >= 0 && ligneD <= MAX)
-				&& (ligneA != ligneD || colonneA != colonneD)) {
-
-			if (!estLibre(ligneA, colonneA) && plateau[ligneA][colonneA].peutAllerEn(ligneD, colonneD, this)) {
-				plateau[ligneA][colonneA].déplacer(this, ligneD, colonneD);
-				if(estLibre(ligneD,colonneD)) {
-					System.out.println("y'a heja mon reuf");
-					return false;
-				}
-				if (plateau[ligneD][colonneD].metEnEchec(this)) {
-					System.out.println("Vous avez mis le roi adverse en situation d'echec");
-				}
-				System.out.println("Le coup a marché !");
-				return true;
-			} else
-				System.out.println("Le coup n'est pas valable");
+		if (plateau[ligneA][colonneA].peutAllerEn(ligneD, colonneD, this)) {
+			plateau[ligneA][colonneA].déplacer(this, ligneD, colonneD);
+			if (estLibre(ligneD, colonneD)) {
+				System.out.println("Une pièce est deja sur la destination");
+				return false;
+			}
+			if (plateau[ligneD][colonneD].metEnEchec(this)) {
+				System.out.println("Vous avez mis le roi adverse en situation d'echec");
+			}
+			System.out.println("Le coup a marché !");
+			return true;
+		} else {
+			System.out.println("Le coup n'est pas valable");
 			return false;
-		} else
-			System.out.println("La position est wtf les amis");
-		return false;
+		}
 	}
 
 	public boolean estLibre(int ligne, int colonne) {
